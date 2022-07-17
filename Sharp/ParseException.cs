c47ae4555fd539b81ac57d7421d0927926daeb08
Brian Elder <gth747m@gmail.com>
2022-07-17 07:39:31 -0400

    Save

---
+++ Sharp/ParseException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp
+{
+    public class ParseException : Exception
+    {
+        public ParseException(Token token, string message)
+        {
+            Sharp.Error(token, message);
+        }
+    }
+}
